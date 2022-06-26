dd8dd5c428bff9ca71bd96d1c4b8c5c890cd1e20
Joseph-01 <softjoe40@gmail.com>
2022-06-26 18:11:02 +0100

    about to implement oauth20

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express")
+const passport = require("passport")
+const session = require("express-session")
 
 const app = express()
+require("./config/passport")(passport)
+
+// Sessions
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: process.env.MONGO_URI, }),
+    })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-})
-
+//export app for test purpose and it is been used in ./bin/www.js
 module.exports = app
