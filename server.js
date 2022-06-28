f04e60d41574f36f6ba77d8258e98acc6e7d5e1b
Joseph-01 <softjoe40@gmail.com>
2022-06-29 00:10:27 +0100

    oauth redirect working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const passport = require("passport")
 const session = require("express-session")
+// const mongoose = require("mongoose")
 
 const app = express()
 require("./config/passport")(passport)
@@ -11,13 +12,23 @@ app.use(
         secret: 'keyboard cat',
         resave: false,
         saveUninitialized: false,
-        store: MongoStore.create({ mongoUrl: process.env.MONGO_URI, }),
+        // store: MongoStore.create({ mongoUrl: process.env.MONGO_URI, }),
     })
 )
 
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+const u = "/auth/google"
+//our ui to integrate oauth
+app.get("/", (req, res) => {
+    //this is where we put our ui url that is going to use our auth service
+    res.send(`<a href=${u}>hello</a>`)
+})
+
+//routes
+app.use("/auth", require("./routes/auth"))
 
 
 //export app for test purpose and it is been used in ./bin/www.js
